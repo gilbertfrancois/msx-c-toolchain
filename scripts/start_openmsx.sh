adb355b23495b8ac2d39dbe3632b0016344b1977
Gilbert Francois Duivesteijn <info@gilbertfrancois.com>
2023-06-29 13:36:51 +0200

    Updates openmsx startup scripts

--- scripts/start_openmsx.sh
+++ scripts/start_openmsx.sh
@@ -1,13 +1,9 @@
 #!/bin/bash	
-# OpenMSX Start Script
-# The emulator will be started only if it's not already active
-
 
 xopenmsx=`ps x | grep "openmsx " | grep -v grep`
  
 if [ "${xopenmsx}" == "" ]; then
-  echo "...Now Starting OpenMsx "
-  /Applications/openMSX.app/Contents/MacOS/openmsx -script ./scripts/MSX2_emul_start_config.txt
+  /Applications/openMSX.app/Contents/MacOS/openmsx -script ./scripts/MSX1_DOS1.txt
 else
-  echo "...openMSX already running "
+  echo "openMSX is already running. Exiting now."
 fi
